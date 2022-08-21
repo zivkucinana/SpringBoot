2f559e72bf32dc355395d605d27e5b6b2ae6b888
zivkucinana <73586507+zivkucinana@users.noreply.github.com>
2022-08-21 18:26:28 +0200

    Redirect

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,5 +1,19 @@
+
+
+
+
+/*--
+
 export const OBRAZOVANJE_URL= "http://localhost:8083/obrazovanje"
 export const PREDUZECE_URL= "http://localhost:8083/preduzece"
 export const SEKTOR_URL= "http://localhost:8083/sektor"
 export const RADNIK_URL= "http://localhost:8083/radnik"
-export const RADNICI_ZA_SEKTOR_URL= "http://localhost:8083/sektorZaRadnikaId"
+export const RADNICI_ZA_SEKTOR_URL= "http://localhost:8083/sektorZaRadnikaId"--*/
+
+
+
+export const OBRAZOVANJE_URL= "https://app31-2019.herokuapp.com/obrazovanje"
+export const PREDUZECE_URL= "https://app31-2019.herokuapp.com/preduzece"
+export const SEKTOR_URL= "https://app31-2019.herokuapp.com/sektor"
+export const RADNIK_URL= "https://app31-2019.herokuapp.com/radnik"
+export const RADNICI_ZA_SEKTOR_URL= "https://app31-2019.herokuapp.com/sektorZaRadnikaId"
